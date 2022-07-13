9dbb944f5a32e713a993751197d83eab450a9813
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-13 18:01:31 -0400

    added moment.js library to format the date. formated the date and updated the props to the variable of the new date. learned more about using moment.js along with understanding props.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Moment from 'moment'
 
 class Movie extends Component {
   render() {
     // let url = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + this.props.backdrop_path
     // console.log(url)
 
-    let formatedDate = this.props.released.toLocaleDateString()
+    let formatedDate = Moment(this.props.released).format("MMM Do YY")
     console.log(formatedDate)
 
     return (
@@ -15,7 +16,7 @@ class Movie extends Component {
           <figcaption>
             <h2>{this.props.title}</h2>
             <p>{this.props.overview}</p>
-            <p>{this.props.released}</p>
+            <p>{formatedDate}</p>
           </figcaption>
         </figure>
       </section>
